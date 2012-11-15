a33e5926a3e32d090dd76f5d798fade9d27839ca
Brendan Kiburg <kiburg@mac-120724.dhcp.fnal.gov>
2012-11-15 15:07:21 -0600

    Added two sample geometry files

---
+++ geometry/WedgeGeom.cc
@@ -0,0 +1,14 @@
+#include "WedgeGeom.hh"
+#include <iostream>
+gm2ringsim::WedgeGeometry::WedgeGeometry(std::string detName) :
+  GeometryBase(detName),
+  length(p_.get<int>("length")),
+  angle(p_.get<double>("angle"))
+{ }
+
+void gm2ringsim::WedgeGeometry::Print(){
+  std::cout<<"\nPrinting Geometry for "<<Name<<std::endl;
+  std::cout<<"\tlength: "<<length<<std::endl;
+  std::cout<<"\tangle: "<<angle<<std::endl;
+  std::cout<<"End Geometry for "<<Name<<std::endl<<std::endl;
+}
