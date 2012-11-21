5e2da4092e5d2ec26aa48b03d437050b6995277f
Brendan <kiburg@fnal.gov>
2012-11-20 23:41:34 -0600

    Prepped PGA.hh until I ran into problems with Root include

--- actions/PrimaryGeneratorAction_service.hh
+++ actions/PrimaryGeneratorAction_service.hh
@@ -16,30 +16,51 @@
       
 */
 
+// Boilerplate for ART Service
 #include "fhiclcpp/ParameterSet.h"
 #include "art/Framework/Services/Registry/ActivityRegistry.h"
 
 // Get the base class
 #include "artg4/actionBase/PrimaryGeneratorActionBase.hh"
 
+// Get the Geant4 code
 #include "Geant4/G4Event.hh"
 #include "Geant4/G4ParticleGun.hh"
+#include "Geant4/G4RotationMatrix.hh"
+//FIXME //#include "Root/TNtuple.h"
+
+// Get the helper files
+//FIXME //#include "gm2ringsim/actions/g2GeneralParticleSource.hh"
+
 
 namespace gm2ringsim
 {
-    class PrimaryGeneratorAction : public artg4::PrimaryGeneratorActionBase
-    {
-        public:
-            PrimaryGeneratorAction(fhicl::ParameterSet const&, art::ActivityRegistry&);
-            virtual ~PrimaryGeneratorAction() { delete particleGun_; }
-      
-            void initialize() override;
+  class PrimaryGeneratorAction : public artg4::PrimaryGeneratorActionBase
+  {
+  public:
+    PrimaryGeneratorAction(fhicl::ParameterSet const&, art::ActivityRegistry&);
+    virtual ~PrimaryGeneratorAction() { delete particleGun_; }
+    
+    void initialize() override;
+    void generatePrimaries( G4Event* ) override;
+    G4double randXFromE821();
+    G4double randTFromE989();
 
-            void generatePrimaries( G4Event* ) override;
-        
-        private:
-            G4ParticleGun* particleGun_;
-    };   
+   
+  private:
+    G4ParticleGun* particleGun_;
+    //FIXME //    g2GeneralParticleSource* g2GPS_;
+    G4ParticleGun* muonGasGun_;
+    G4ParticleGun* inflectorGun;
+
+    //  NSF (30 AUG '12): Why is this here? 
+    G4RotationMatrix m_localToGlobalRot ;
+    G4ThreeVector m_yhatGlobal, m_caloCenterGlobal ;
+    //FIXME   //  TNtuple* m_nt ;
+    int m_ntCount, m_ntTot ;
+    float m_elex, m_eley, m_elez ;
+    float m_elpx, m_elpy, m_elpz ;
+  };   
 }
 #endif
 
