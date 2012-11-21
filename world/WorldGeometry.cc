c8896452fc87bd506b309cd3ed3373f22288b19c
Adam Lyon <lyon@fnal.gov>
2012-11-20 23:26:42 -0600

    Use the dimensions of the lab from g2migtrace (but in mm)

--- world/WorldGeometry.cc
+++ world/WorldGeometry.cc
@@ -2,19 +2,21 @@
 
 #include "gm2ringsim/world/WorldGeometry.hh"
 #include "messagefacility/MessageLogger/MessageLogger.h"
+#include "Geant4/globals.hh"
 #include <iostream>
 
 gm2ringsim::WorldGeometry::WorldGeometry(std::string const & detName) :
   GeometryBase(detName),
-  world_x(p.get<double>("world_x")),
-  world_y(p.get<double>("world_y")),
-  world_z(p.get<double>("world_z"))
+  world_x(p.get<double>("world_x") * mm),
+  world_y(p.get<double>("world_y") * mm),
+  world_z(p.get<double>("world_z") * mm)
 {}
 
 void gm2ringsim::WorldGeometry::print() const {
   
   // Use the MessageLogger (see https://cdcvs.fnal.gov/redmine/projects/novaart/wiki/Using_the_Framework#MessageLogger )
-  mf::LogInfo("WorldGeometry") << "World geometry is world_x=" << world_x
+  mf::LogInfo("WorldGeometry") << "World geometry is"
+                             << " world_x=" << world_x
                              << " world_y=" << world_y
                              << " world_z=" << world_z;  
 }
