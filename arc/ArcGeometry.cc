05ddebe9b811cdeefeeca2bb2b595d932f586e89
Adam Lyon <lyon@fnal.gov>
2012-11-21 23:38:41 -0600

    Make geometry output prettier

--- arc/ArcGeometry.cc
+++ arc/ArcGeometry.cc
@@ -18,12 +18,12 @@ gm2ringsim::ArcGeometry::ArcGeometry(std::string const & detName) :
 void gm2ringsim::ArcGeometry::print() const {
   
   // Use the MessageLogger (see https://cdcvs.fnal.gov/redmine/projects/novaart/wiki/Using_the_Framework#MessageLogger )
-  mf::LogInfo("ArcGeometry") << "Arc geometry is"
-  << " arc_rMin="         << arc_rMin
-  << " arc_rMax="         << arc_rMax
-  << " arc11_rExtension=" << arc11_rExtension
-  << " arc_z="            << arc_z
-  << " arc_Sphi="         << arc_Sphi
-  << " arc_Dphi="         << arc_Dphi;
+  mf::LogInfo("ArcGeometry") << "Arc geometry is" << "\n"
+  << "    arc_rMin="         << arc_rMin << "\n"
+  << "    arc_rMax="         << arc_rMax << "\n"
+  << "    arc11_rExtension=" << arc11_rExtension << "\n"
+  << "    arc_z="            << arc_z << "\n"
+  << "    arc_Sphi="         << arc_Sphi << "\n"
+  << "    arc_Dphi="         << arc_Dphi;
 }
 
