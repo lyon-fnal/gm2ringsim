96be5a7526b60d4f0282c9468b21b86e9ef00ef0
Brendan <kiburg@fnal.gov>
2012-11-24 16:09:19 -0600

    Finished implementing StackingAction_service, with sample fcl file added to debug.fcl

--- actions/StackingAction_service.cc
+++ actions/StackingAction_service.cc
@@ -6,15 +6,56 @@
 #include "art/Framework/Services/Registry/ServiceMacros.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
+// Geant4 includes
+#include "Geant4/G4Track.hh"
+#include "Geant4/G4UnitsTable.hh"
+
 
 gm2ringsim::StackingAction::StackingAction(fhicl::ParameterSet const& p, art::ActivityRegistry&) :
-    artg4::StackingActionBase(p.get<std::string>("name"))
+  artg4::StackingActionBase(p.get<std::string>("name")),
+  minWavelength_(p.get<double>("minWavelength",250)), //nm
+  maxWavelength_(p.get<double>("maxWavelength",950)) //nm
 {}
 
 gm2ringsim::StackingAction::~StackingAction(){
 }
 
-bool gm2ringsim::StackingAction::killNewTrack( const G4Track* ) { return false; }
+bool gm2ringsim::StackingAction::killNewTrack( const G4Track* ) { 
+  return false; 
+}
+
+G4ClassificationOfNewTrack gm2ringsim::StackingAction::ClassifyNewTrack(const G4Track* aTrack){
+  // pass all non-optical tracks as normal: urgent stacking
+  int pdg = aTrack->GetDefinition()->GetPDGEncoding();
+  
+  // Use only for muon tracker (TG)
+  //if ( pdg == 13 || pdg == -13 ) { return( fUrgent ); }
+  //else { return( fKill ); }
+  
+  if ( pdg != 0 ) { return fUrgent; }
+
+  // Kill optical photons that are outside the range of interest
+  double wavelength = 0.001240 / ( aTrack->GetTotalEnergy() / MeV ) ;
+  if ( wavelength < minWavelength_ || wavelength > maxWavelength_ ) {
+    return fKill;
+  } else {
+    return fUrgent;
+  }
+
+  return fUrgent;
+
+}
+
+/** Interface to set accepted wavelength range **/
+void gm2ringsim::StackingAction::setMinimumOpticalWavelength(G4double minWavelength ) {
+  minWavelength_ = minWavelength;
+
+}
+void gm2ringsim::StackingAction::setMaximumOpticalWavelength(G4double maxWavelength ) {
+  maxWavelength_ = maxWavelength;
+}
+
+
 
 using gm2ringsim::StackingAction;
 DEFINE_ART_SERVICE(StackingAction)
