7217015a0178b2d4dcf120ff2f87ce0d66cafcc8
Leah Welty-Rieger <lwrieger@gmail.com>
2012-11-29 17:33:33 -0600

    This code compiles, but then when trying to build the logical volumes for the stations it seg faults.
    Added in more Geometry parameters, and then the station service.

--- station/StationGeometry.cc
+++ station/StationGeometry.cc
@@ -8,8 +8,34 @@ gm2ringsim::StationGeometry::StationGeometry(std::string const & detName) :
   GeometryBase(detName),
   r( p.get<double>("r") * mm),
   v( p.get<double>("v") * mm),
-  t( p.get<double>("t") * mm){
+  t( p.get<double>("t") * mm),
+  r_in( p.get<double>("r_in") * in),
+  r_out( p.get<double>("r_out") * in),
+  theta_out( p.get<std::vector<double>>("theta_out") ),
+  window_angle( p.get<double>("window_angle") * deg),
+  t_offset( p.get<double>("t_offset")),
+  r_offset( p.get<double>("r_offset")),
+  v_offset( p.get<double>("v_offset")),
+  t_rotation( p.get<double>("t_rotation")),
+  r_rotation( p.get<double>("r_rotation")),
+  v_rotation( p.get<double>("v_rotation")),
+  r_c((r_in + r_out)/2)
+{
+
+  for (auto& entry : theta_out ) { entry *= deg; }
+  for (auto& entry : theta_in ) { entry *= deg; }
+  for (auto& entry : theta_c ) { entry *= deg; }
     
+  //Derived quantities
+  std::vector<double> theta_in;
+  std::vector<double> theta_c;
+  
+  theta_in[0] = theta_out[0] - 0.12*deg;
+  theta_in[1] = theta_out[1] - 0.12*deg;
+		
+  theta_c[0] = (theta_in[0] + theta_out[0])/2;
+  theta_c[1] = (theta_in[1] + theta_in[1])/2;
+  
 }
 
 void gm2ringsim::StationGeometry::print() {
@@ -17,6 +43,12 @@ void gm2ringsim::StationGeometry::print() {
   oss << "  r=" << r << "\n";
   oss << "  v=" << v << "\n";
   oss << "  t=" << t << "\n";
+	
+	oss << "  r_in=" << r_in << "\n";
+	oss << "  r_out=" << r_out << "\n";
+	oss << "  r_c=" << r_c << "\n";
+
+	
   mf::LogInfo("STATIONGEOMETRY") << oss.str();
 
 }
--- station/StationGeometry.hh
+++ station/StationGeometry.hh
@@ -7,16 +7,35 @@
 #include "gm2ringsim/common/conversions.hh"
 #include <iostream>
 
-
 namespace gm2ringsim{
   struct StationGeometry : public artg4::GeometryBase {
     StationGeometry(std::string const &);
-    
+    void print();
+
+    double const in = 25.4 * mm;
+
     const double r;
     const double v;
     const double t;
+    const double r_in;
+    const double r_out;
+    std::vector<double> theta_out;
+
+    const double window_angle;
+
+    const double t_offset;
+    const double r_offset;
+    const double v_offset;
+    const double t_rotation;
+    const double r_rotation;
+    const double v_rotation;
     
-    void print();
+
+    //Derived constants
+    double r_c;
+    std::vector<double> theta_in;
+    std::vector<double> theta_c;
+    
   };
 
 }
