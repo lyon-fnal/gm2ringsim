8febb9b677ce1462885f9de8fe18526e2b8639c1
Brendan <kiburg@fnal.gov>
2012-12-07 00:40:28 -0600

    Artified some of the Spin classes

--- common/spin/classes.h
+++ common/spin/classes.h
@@ -3,10 +3,10 @@
 #include <vector>
 
 #include "art/Persistency/Common/Wrapper.h"
-#include "gm2ringsim/common/ring/RingArtRecord.hh"
+#include "gm2ringsim/common/spin/SpinArtRecord.hh"
 
 // Template the vector (don't use typedef)
-template class std::vector< gm2ringsim::RingArtRecord >;
+template class std::vector< gm2ringsim::SpinArtRecord >;
 
 // Template the wrapper for the vector (typedef okay)
-template class art::Wrapper< gm2ringsim::RingArtRecordCollection >;
+template class art::Wrapper< gm2ringsim::SpinArtRecordCollection >;
