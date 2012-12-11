77134ac3771237e9768f804dc714c5bc35820108
Peter Winter <winterp@anl.gov>
2012-12-11 08:43:56 -0600

    More collimator work

--- collimator/Collimator_service.cc
+++ collimator/Collimator_service.cc
@@ -1,6 +1,7 @@
 // Implementation of Collimator
 
 #include "gm2ringsim/collimator/Collimator_service.hh"
+#include "gm2ringsim/common/g2PreciseValues.cc"
 
 #include "artg4/material/Materials.hh"
 #include "art/Framework/Services/Registry/ServiceMacros.h"
@@ -33,7 +34,7 @@ std::vector<G4LogicalVolume *> gm2ringsim::Collimator::doBuildLVs() {
   
   // Loop over all 9 collimators
   for(G4int collNumber = 0; collNumber < geom_.nCollimators; collNumber++){
-    if(geom_.collimatorType[collNumber] == OFF) continue;
+    if(geom_.collimatorType[collNumber] == OFF) collLVs.push_back(0);
     
     G4Tubs *collimator_S = new G4Tubs("coll", 
 				      geom_.coll_rMin, 
@@ -56,10 +57,43 @@ std::vector<G4LogicalVolume *> gm2ringsim::Collimator::doBuildLVs() {
 }
 
 // Build the physical volumes
-std::vector<G4VPhysicalVolume *> gm2ringsim::Collimator::doPlaceToPVs( std::vector<G4LogicalVolume*>) {
+std::vector<G4VPhysicalVolume *> gm2ringsim::Collimator::doPlaceToPVs( std::vector<G4LogicalVolume*> vacWalls) {
+  G4double const theta1  = 0., phi = pi/2., theta2 = 0.;
   std::vector<G4VPhysicalVolume *> collPVs;
 
-  //char objectName[30];
+  char objectName[30];
+  
+  for(G4int collNumber = 0; collNumber < geom_.nCollimators; collNumber++){
+    if(geom_.collimatorType[collNumber] == OFF) continue;
+
+    if (vacWalls[geom_.cVacWallArray[collNumber]]->GetNoDaughters() != 1 ) {
+      throw cet::exception("Collimator_service") << "What?? My wall has no vacuum! Abort! \n";
+    }
+    
+    // If collimator is a half collimator on the outside of the storage
+    // ring, then use this angle for Euler angle theta2
+    G4double theta = theta2;
+    if(geom_.collimatorType[collNumber] == HALF_LRO)
+      theta = pi;
+ 
+    // Get the vacuum chamber logical volume from the wall
+    G4LogicalVolume* vacLV = vacWalls[geom_.cVacWallArray[collNumber]]->GetDaughter(0)->GetLogicalVolume();
+   
+    std::sprintf(objectName, "%s-%02d", e_collimator_names[geom_.collimatorType[collNumber]],  collNumber);
+
+    /** @bug tCB_Dphi from vacChamberParameters.hh should be used
+	instead of 0.01*degree, but we have to fix the multiple
+	inclusion problem. */
+    collPVs.push_back(new G4PVPlacement(new G4RotationMatrix(theta1+0.01*degree, phi, theta),
+					G4ThreeVector(R_magic(),
+						      geom_.coll_z+R_magic()*sin(0.01*degree),
+						      0*m),
+					lvs()[collNumber],
+					objectName,
+					vacLV,
+					false,
+					collNumber));
+  }
   
   return collPVs;
 }
