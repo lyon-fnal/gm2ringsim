0284b692ae23f293a600dabd655869b28a6c2c50
Robin Bjorkquist <rb532@cornell.edu>
2013-03-13 10:54:28 -0400

    Add classes.h and classes_def.xml for photodetector hits.

---
+++ calo/classes.h
@@ -0,0 +1,16 @@
+// classes.h
+
+#include <vector>
+
+#include "art/Persistency/Common/Wrapper.h"
+#include "gm2ringsim/calo/PhotodetectorArtRecord.hh"
+#include "gm2ringsim/calo/PhotodetectorPhotonArtRecord.hh"
+
+// Template the vector (don't use typedef)
+template class std::vector< gm2ringsim::PhotodetectorArtRecord >;
+template class std::vector< gm2ringsim::PhotodetectorPhotonArtRecord >;
+
+// Template the wrapper for the vector (typedef okay)
+template class art::Wrapper< gm2ringsim::PhotodetectorArtRecordCollection >;
+template class art::Wrapper< gm2ringsim::PhotodetectorPhotonArtRecordCollection >;
+
