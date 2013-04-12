f40f534e8eabe4ff9789b796b0c70e7324a70788
Robin Bjorkquist <rb532@cornell.edu>
2013-04-12 12:24:03 -0400

    Create particle gun that reads calo hits; still need to read in particle type (for now always makes electrons).

--- actions/PGA/CaloHitPGA_service.cc
+++ actions/PGA/CaloHitPGA_service.cc
@@ -1,9 +1,96 @@
-//
-//  CaloHitPGA_service.cc
-//  ringsim_xcode
-//
-//  Created by Robin Bjorkquist on 3/28/13.
-//  Copyright (c) 2013 Robin Bjorkquist. All rights reserved.
-//
+/** @file CaloHitPGA_service.cc
+ 
+ Based on SimpleParticleSource_service.cc
+ This will generate electrons aimed at one of the calorimeters.
+ Simple version with hard-coded position, momentum, and particle energy for now.
+ 
+ @author  Robin Bjorkquist
+ @date    2013
+ 
+ */
 
-#include "CaloHitPGA_service.h"
+// Get the PGA header
+#include "gm2ringsim/actions/PGA/CaloHitPGA_service.hh"
+
+
+// ART includes
+#include "art/Framework/Services/Registry/ServiceMacros.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art/Framework/Principal/Event.h"
+
+#include "artg4/services/ActionHolder_service.hh"
+#include "artg4/services/DetectorHolder_service.hh"
+#include "artg4/Core/DetectorBase.hh"
+
+#include "gm2ringsim/calo/CaloArtRecord.hh"
+
+// Geant includes
+#include "Geant4/G4ParticleTable.hh"
+#include "Geant4/G4ParticleDefinition.hh"
+#include "Geant4/G4RotationMatrix.hh"
+#include "Geant4/G4LogicalVolume.hh"
+
+
+// Constructor
+gm2ringsim::CaloHitPGA::CaloHitPGA(fhicl::ParameterSet const& p, art::ActivityRegistry&) :
+artg4::PrimaryGeneratorActionBase(p.get<std::string>("name", "CaloHitPGA")),
+particleGun_( 0 )     // Must not initialize here because Geant isn't ready yet
+{}
+
+
+// Destructor
+gm2ringsim::CaloHitPGA::~CaloHitPGA(){
+    delete particleGun_;
+}
+
+
+// initialize
+void gm2ringsim::CaloHitPGA::initialize() {
+    
+    particleGun_ = new G4ParticleGun();
+    
+    // Just use electrons for now -- need to change to particle type of each calo hit
+    
+    G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+    G4ParticleDefinition* minusElec = particleTable->FindParticle("e-");
+    particleGun_->SetParticleDefinition(minusElec);
+    
+}
+
+
+// Generate Primaries
+void gm2ringsim::CaloHitPGA::generatePrimaries(G4Event* evt) {
+    
+    // get the event
+    art::ServiceHandle<artg4::ActionHolderService> actionHolder;
+    art::Event &e = actionHolder->getCurrArtEvent();
+    
+    // get calo hits from the event
+    art::Handle<CaloArtRecordCollection> caloHitHandle;
+    e.getByLabel("artg4", "calorimeter", caloHitHandle);
+    const CaloArtRecordCollection & caloHits = *caloHitHandle;
+    
+    particleGun_ -> SetNumberOfParticles( caloHits.size() );
+    
+    // initiate primary vertex for each calo hit
+    for (auto entry : caloHits)
+    {        
+        G4ThreeVector position( entry.x, entry.y, entry.z );
+        particleGun_ -> SetParticlePosition( position );
+        
+        G4ThreeVector momentum( entry.px, entry.py, entry.pz );
+        particleGun_ -> SetParticleMomentum( momentum );
+        
+        particleGun_ -> SetParticleTime( entry.time );
+        
+        particleGun_->GeneratePrimaryVertex( evt );
+
+    }
+    
+
+    
+    
+}
+
+using gm2ringsim::CaloHitPGA;
+DEFINE_ART_SERVICE(CaloHitPGA)
--- actions/PGA/CaloHitPGA_service.hh
+++ actions/PGA/CaloHitPGA_service.hh
@@ -1,14 +1,54 @@
-//
-//  CaloHitPGA_service.h
-//  ringsim_xcode
-//
-//  Created by Robin Bjorkquist on 3/28/13.
-//  Copyright (c) 2013 Robin Bjorkquist. All rights reserved.
-//
+#ifndef CALOHITPGA_SERVICE_HH
+#define CALOHITPGA_SERVICE_HH
 
-#ifndef __ringsim_xcode__CaloHitPGA_service__
-#define __ringsim_xcode__CaloHitPGA_service__
+/** @file CaloHitPGA_service.hh
+ 
+ Based on SimpleParticleSource_service.hh
+ 
+ This particle gun extracts the calo hits from the input file. For each calo hit, it creates a primary vertex 
+ with the stored particle type, position, and momentum.
+ 
+ This allows the simulation to be done in two stages:
+ 
+    1. Do simulations with calorimeter tracking turned off; tracks entering the calorimeter volume are killed 
+       and stored in calo hits.
+ 
+    2. Use this particle gun to do the full calorimeter simulation for the tracks previously stored.
+ 
+ @author  Robin Bjorkquist
+ @date    2013
+ 
+ */
 
-#include <iostream>
+// Boilerplate for ART Service
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Services/Registry/ActivityRegistry.h"
 
-#endif /* defined(__ringsim_xcode__CaloHitPGA_service__) */
+// Get the base class
+#include "artg4/actionBase/PrimaryGeneratorActionBase.hh"
+
+// Get the Geant4 code
+#include "Geant4/G4Event.hh"
+#include "Geant4/G4ParticleGun.hh"
+
+namespace gm2ringsim
+{
+    class CaloHitPGA : public artg4::PrimaryGeneratorActionBase
+    {
+    public:
+        CaloHitPGA(fhicl::ParameterSet const&, art::ActivityRegistry&);
+        virtual ~CaloHitPGA();
+        
+        void initialize() override;
+        
+        void generatePrimaries( G4Event* ) override;
+        
+    private:
+        G4ParticleGun* particleGun_;
+        
+    }; // End CaloHitPGA class
+    
+} // gm2ringsim namespace
+
+#endif // CALOHITPGA_SERVICE_HH
+
