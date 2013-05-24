f6903a11fc29fade4fbb7dddd7d5ef33ef7a1d85
Leah Welty-Rieger <lwrieger@gmail.com>
2013-05-24 09:34:33 -0500

    Fix for the angle rotation of the straws and added and almost edge on view of the straws in the vis.mac

--- strawtracker/Straws_service.cc
+++ strawtracker/Straws_service.cc
@@ -155,7 +155,7 @@ std::vector<G4VPhysicalVolume *> gm2ringsim::Straws::doPlaceToPVs( std::vector<G
     y= geom_.y_position[row];
     
     G4RotationMatrix* yRot = new G4RotationMatrix;
-    double rot = geom_.layer_angle*deg;
+    double rot = geom_.layer_angle;
     if(row > 1) rot = -rot;
     yRot -> rotateY(rot);
     G4ThreeVector placement(x, y, 0);
