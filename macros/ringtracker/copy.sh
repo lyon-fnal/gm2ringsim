9cc6b31a7d8e8905ddfe113b3fba0bcea8d2d7b6
Thomas <tgadfort@fnal.gov>
2013-11-01 13:47:05 -0500

    Saving new beta functions and macro to do something with them.

--- macros/ringtracker/copy.sh
+++ macros/ringtracker/copy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 targetdir="../../fcl/"
-filestomove="allrun.sh   makefcl.sh   move.sh  plotinflector.C  plotinf.sh  run.sh  submit.sh  plotfastrotation.C  plotfast.sh fasrof.cxx g2gen.cxx sub2.sh FastRotation.C runner.sh"
+filestomove="allrun.sh   makefcl.sh   move.sh  plotinflector.C  plotinf.sh  run.sh  submit.sh  plotfastrotation.C  plotfast.sh fasrof.cxx g2gen.cxx sub2.sh FastRotation.C runner.sh plotTwiss.C twiss_n18.dat 
+twiss_n137.dat"
 
 for file in ${filestomove}; do
     echo "cp -f ${targetdir}/${file} ."
