704fcd4ce3bf019a5afab18aa2693a409a706684
Leah Welty-Rieger <lwrieger@gmail.com>
2014-03-31 11:21:50 -0500

    Updates to the how the sizes of the manifolds are calculated

--- strawtracker/StrawTracker_service.cc
+++ strawtracker/StrawTracker_service.cc
@@ -51,8 +51,8 @@ std::vector<G4LogicalVolume *> gm2ringsim::StrawTracker::doBuildLVs() {
       G4VSolid *strawStation = new G4Box("strawSystem", geom_.strawStationSizeHalf[sc], geom_.strawStationWidthHalf[sc], geom_.strawStationHeightHalf);
       
       //Build Manifold structure. Hollow box built by subtracting a slightly smaller box from the full size. 
-      G4VSolid *outerStationManifold = new G4Box("outerManifoldSystem", geom_.strawStationSizeHalf[sc], geom_.strawStationWidthHalf[sc], geom_.strawStationManifoldHeightHalf);
-      G4VSolid *innerStationManifold = new G4Box("innerManifoldSystem", geom_.strawStationSizeHalf[sc]-geom_.strawStationManifoldThickness, geom_.strawStationWidthHalf[sc]-geom_.strawStationManifoldThickness, geom_.strawStationManifoldHeightHalf-geom_.strawStationManifoldThickness);
+      G4VSolid *outerStationManifold = new G4Box("outerManifoldSystem", geom_.strawStationSizeHalf[sc], geom_.strawStationManifoldWidth/2, geom_.strawStationManifoldHeightHalf);
+      G4VSolid *innerStationManifold = new G4Box("innerManifoldSystem", geom_.strawStationSizeHalf[sc]-geom_.strawStationManifoldThickness, geom_.strawStationManifoldWidth/2-geom_.strawStationManifoldThickness, geom_.strawStationManifoldHeightHalf-geom_.strawStationManifoldThickness);
       G4SubtractionSolid *stationManifold = new G4SubtractionSolid("stationManifold", outerStationManifold, innerStationManifold);
 
 
